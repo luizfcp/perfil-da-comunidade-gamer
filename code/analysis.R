e1734e8d09d0309309ecb4f60f990fad6b8dc130
Lyncoln <32916183+Lyncoln@users.noreply.github.com>
2019-06-25 10:05:31 -0300

    Update analysis.R

--- code/analysis.R
+++ code/analysis.R
@@ -1,1 +1,13 @@
 # jogos brasil, xbox one br, jogos mobile br/pt, uff niteroi, ilha dos estudos, ilha da macacada
+
+library(dplyr);
+questionario = read_csv("https://github.com/luizfcp/perfil-da-comunidade-gamer/raw/master/data/Perfil%20da%20Comunidade%20Gamer%20(respostas)%20-%20Respostas%20ao%20formul%C3%A1rio%201.csv")
+  
+console = questionario %>% 
+  filter(`Qual plataforma prefere usar para jogar?` == "Console (video game)")
+computador = questionario %>% 
+  filter(`Qual plataforma prefere usar para jogar?` == "Desktop (PC de mesa)")
+notebook = questionario %>% 
+  filter(`Qual plataforma prefere usar para jogar?` == "Desktop (PC de mesa)")
+celular = questionario %>% 
+  filter(`Qual plataforma prefere usar para jogar?` == "Outros (Celular)")
