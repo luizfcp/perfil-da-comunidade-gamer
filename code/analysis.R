56e74c4eabfbcd8c90f6ff89e381c808a8a690d3
luizfcp <luizfcpassos@hotmail.com>
2019-06-27 13:42:49 +0000

    analysis

--- code/analysis.R
+++ code/analysis.R
@@ -1,4 +1,3 @@
-
 library(dplyr);
 questionario = read_csv("https://github.com/luizfcp/perfil-da-comunidade-gamer/raw/master/data/Perfil%20da%20Comunidade%20Gamer%20(respostas)%20-%20Respostas%20ao%20formul%C3%A1rio%201.csv")
   
@@ -10,4 +9,3 @@ notebook = questionario %>%
   filter(`Qual plataforma prefere usar para jogar?` == "Notebook")
 celular = questionario %>% 
   filter(`Qual plataforma prefere usar para jogar?` == "Outros (Celular)")
-
